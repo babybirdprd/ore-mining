b8a6b5f91128683eb16124c650c418a00cc76305
Dafiq(Xavi_Hernandez) <67586661+JaviAnibarro@users.noreply.github.com>
2024-04-04 06:26:40 +0700

    Create mining-ore.sh

---
+++ mining-ore.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while true; do
+    ore --rpc https://api.mainnet-beta.solana.com/ --keypair ~/.config/solana/id.json --priority-fee 1 mine --threads 4
+
+    if [ $? -eq 0 ]; then
+        break
+    else
+        echo "Terjadi kesalahan. Menunggu sebentar sebelum mencoba lagi..."
+        sleep 5
+    fi
+done
